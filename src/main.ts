76430a1f159d7617a50ae5534ebada413a6015b7
rkoren1 <rok.koren@netica.si>
2023-06-21 13:52:56 +0200

    init

---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Automate SL API')
+    .setDescription('Automate SL API')
+    .setVersion('1.0')
+    .addTag('AutomateSL')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+  await app.listen(3000);
+}
+bootstrap();
