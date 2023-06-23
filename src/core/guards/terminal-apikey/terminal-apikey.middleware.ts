ba194ad0d0f0e3a0ff26d19e3108eb40eba64427
Rok Koren <rok.koren@netica.si>
2023-06-23 14:56:28 +0200

    added terminal apikey middleware

---
+++ src/core/guards/terminal-apikey/terminal-apikey.middleware.ts
@@ -0,0 +1,20 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+
+@Injectable()
+export class TerminalApikeyMiddleware implements NestMiddleware {
+  use(req: any, res: any, next: () => void) {
+    const apiKey = req.query.apiKey;
+    Terminal.findOne({
+      attributes: ['name', 'apiKey'],
+      where: { apiKey: apiKey },
+    })
+      .then((terminal) => {
+        if (terminal === undefined || apiKey !== terminal.apiKey) {
+          return res.status(401).json({ message: 'Unauthorized' });
+        }
+        req.terminalName = terminal.name;
+        next();
+      })
+      .catch((err) => res.status(401).json({ message: 'Unauthorized' }));
+  }
+}
