9de2db2d0046bea33b1be98e39bfe16241eedc0d
Rok Koren <rok.koren@netica.si>
2023-06-26 13:38:08 +0200

    cleanup

--- src/modules/discord-settings/discord-settings.service.ts
+++ src/modules/discord-settings/discord-settings.service.ts
@@ -1,26 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { CreateDiscordSettingDto } from './dto/create-discord-setting.dto';
-import { UpdateDiscordSettingDto } from './dto/update-discord-setting.dto';
 
 @Injectable()
-export class DiscordSettingsService {
-  create(createDiscordSettingDto: CreateDiscordSettingDto) {
-    return 'This action adds a new discordSetting';
-  }
-
-  findAll() {
-    return `This action returns all discordSettings`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} discordSetting`;
-  }
-
-  update(id: number, updateDiscordSettingDto: UpdateDiscordSettingDto) {
-    return `This action updates a #${id} discordSetting`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} discordSetting`;
-  }
-}
+export class DiscordSettingsService {}
