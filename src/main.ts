eb981b1822f2ac1b9b842a48ddf3b53f2aeb1194
Rok Koren <rok.koren@netica.si>
2023-06-29 14:48:17 +0200

    init

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import * as cookieParser from 'cookie-parser';
+import * as fs from 'fs';
 import { AppModule } from './app.module';
 import { discClient } from './core/services/discord-bot.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      http2: true,
+      https: {
+        key: fs.readFileSync('src/private-key.pem'),
+        cert: fs.readFileSync('src/public-certificate.pem'),
+      },
+    }),
+  );
   app.use(cookieParser());
   app.setGlobalPrefix('/api');
   app.enableCors({
@@ -25,7 +39,7 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, document);
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
   discClient.login(process.env.DISC_BOT_TOKEN);
 }
 bootstrap();
